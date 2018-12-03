ba4f9fbd9c4ed9127cc1e62b4e048ea6149c1eb4
Kevin Durbin <kevindurb@gmail.com>
2018-12-03 07:20:41 -0700

    initial stuff

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [
+              ['@babel/plugin-proposal-decorators', { legacy: true }],
+              ['@babel/plugin-proposal-class-properties', { loose: true }],
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
